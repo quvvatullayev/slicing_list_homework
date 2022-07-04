ee4050dbbe6835970055f46ab6d154f60b7a8edb
tajriba <quvvatullayev@gmail.com>
2022-07-04 08:55:41 +0300

    Slicing_list08.py

--- Slicing_list08.py
+++ Slicing_list08.py
@@ -7,4 +7,7 @@ def main(list1,n):
     Returns:
         list: return answer.
     """
-    return list1[::n]
+    list2 = list1[::-1]
+    return list2[::n]
+
+print(main(['a', 1, 'b', 2, 'c', 3, 'd', 4],2))
